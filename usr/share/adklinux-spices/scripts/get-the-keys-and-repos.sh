f256d9120b19197f346593114d078fc7aa4d0da3
Technopig100 <technopig10@gmail.com>
2022-04-23 05:56:37 -0400

    Added new key

--- usr/share/adklinux-spices/scripts/get-the-keys-and-repos.sh
+++ usr/share/adklinux-spices/scripts/get-the-keys-and-repos.sh
@@ -7,12 +7,10 @@ echo "Letting pacman know ADK-Linux package keys"
 sudo pacman-key --recv-keys 0A74C5909A8A2600AD012AF67B22BF2F72D54DAC --keyserver hkps://keyserver.ubuntu.com
 sudo pacman-key --lsign-key 0A74C5909A8A2600AD012AF67B22BF2F72D54DAC
 
+sudo pacman-key --recv-keys DB8C9336927412AD01764AD88C40B4EF4EF24C82 --keyserver hkps://keyserver.ubuntu.com 
+sudo pacman-key --lsign-key DB8C9336927412AD01764AD88C40B4EF4EF24C82 
+
 sudo pacman -S wget --noconfirm --needed
-
-echo "Getting the ADK-Linux keys from the ADK-Linux repo"
-
-sudo wget https://github.com/Technopig100/adk_repo/raw/main/x86_64/adklinux-keyring-20230919-6-any.pkg.tar.zst -O /tmp/adklinux-keyring-20230919-6-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/adklinux-keyring-20230919-6-any.pkg.tar.zst
 
 ######################################################################################################################
 
@@ -23,7 +21,11 @@ echo '
 #ADK Linux repository
 
 #[adklinux_test_repo]
-#Include = /etc/pacman.d/adklinux-mirrorlist
+#Server = https://faireygitea2.duckdns.org/jacob/$repo/raw/branch/master/$arch/
 
 [adklinux_repo]
 Include = /etc/pacman.d/adklinux-mirrorlist' | sudo tee --append /etc/pacman.conf
+
+echo "Getting the ADK-Linux keys from the ADK-Linux repo"
+
+sudo pacman -Syy adklinux-keyring-git --noconfirm --needed
